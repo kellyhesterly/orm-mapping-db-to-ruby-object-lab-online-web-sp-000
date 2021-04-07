de267c73cf4260a02227df65e898bb47fac3bbd0
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 21:16:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,6 @@ class Student
   end
 
   def self.students_below_12th_grade
-    DB[:conn].execute("SELECT * FROM students WHERE grade != 12")
+    DB[:conn].execute("SELECT * FROM students WHERE grade < 12")
   end
 end
