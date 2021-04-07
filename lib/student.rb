e1d5c41d52ce4ab1b5046f2715b54f9777775759
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 21:00:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,6 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name:, grade:, id: nil)
-    @name = name
-    @grade = grade
-    @id = id
-  end
-
   def self.create_table
   end
 
