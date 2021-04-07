5f6aa944fe5074488d1ca05927a5814513acf05c
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 21:26:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
     DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
 
-  def self.first_X_students_in_grade_10(student)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10
     SQL
