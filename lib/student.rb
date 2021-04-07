5421f57b1ca78204ac950ed81053fd5abe758940
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 21:01:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,13 @@ class Student
   attr_accessor :name, :grade, :id
 
   def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    );
+    SQL
   end
 
   def self.drop_table
